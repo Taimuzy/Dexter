303a0bf277330af4c911805184aa1686a4dd9e7f
TechMaker <sesi.techmakerrobotics@gmail.com>
2024-05-31 16:38:53 -0300

    first commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,96 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+
+  private WPI_VictorSPX leftMotor1 = new WPI_VictorSPX(1);
+  private WPI_VictorSPX leftMotor2 = new WPI_VictorSPX(3);
+  private WPI_VictorSPX rightMotor1 = new WPI_VictorSPX(2);
+  private WPI_VictorSPX rightMotor2 = new WPI_VictorSPX(4);
+
+  private CommandXboxController joy1 = new CommandXboxController(0);
+
+  private double startTime;
+
+  public Robot(){
+    leftMotor2.follow(leftMotor1);
+    rightMotor2.follow(rightMotor1);
+  }
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void autonomousInit() {
+    startTime = Timer.getFPGATimestamp();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    double time = Timer.getFPGATimestamp();
+    System.out.println(time - startTime);
+
+    if (time - startTime < 3) {
+      leftMotor1.set(0.6);
+      leftMotor2.set(0.6);
+      rightMotor1.set(-0.6);
+      rightMotor2.set(-0.6);
+    } else {
+      leftMotor1.set(0);
+      leftMotor2.set(0);
+      rightMotor1.set(0);
+      rightMotor2.set(0);
+    }
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    double speed = -joy1.getRawAxis(1) * 0.6;
+    double turn = joy1.getRawAxis(4) * 0.3;
+
+    double left = speed + turn;
+    double right = speed - turn;
+
+    leftMotor1.set(left);
+    leftMotor2.set(left);
+    rightMotor1.set(-right);
+    rightMotor2.set(-right);
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+}
